638d0d3afe31066da6fb88ad5a8d494db1f38df5
LIUFEIFAN1999 <1935245590@qq.com>
2021-03-19 21:38:39 +0800

    第四次作业

---
+++ Homework04/Alarm/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm
+{
+    public delegate void TickHandler();
+    public delegate void AlarmHandler();
+
+    public class Clock
+    {
+        public event TickHandler Tick;
+        public event AlarmHandler Alarm;
+
+        private DateTime nowTime;
+        private DateTime alarmTime;
+
+        public DateTime NowTime { get => nowTime; set => nowTime = value; }
+        public DateTime AlarmTime { get => alarmTime; set => alarmTime = value; }
+
+        public Clock()
+        {
+            nowTime = DateTime.Now;
+        }
+        public void start()
+        {
+            bool isAlarmed = false;
+            Timer timer = new Timer(1000);
+            timer.Elapsed += new ElapsedEventHandler(Ticks);
+            timer.Start();
+            while (true)
+            {
+                nowTime = DateTime.Now;
+                if(nowTime >= alarmTime && !isAlarmed)
+                {
+                    Alarms();
+                    isAlarmed = true;
+                }
+            }
+        }
+        public void Ticks(object sender, ElapsedEventArgs e)
+        {
+            Tick();
+        }
+        public void Alarms()
+        {
+            Alarm();
+        }
+    }
+
+    public class MyClock
+    {
+        public Clock clock = new Clock();
+        public MyClock()
+        {
+            clock.Tick += Clock_Tick;
+            clock.Alarm += Clock_Alarm;
+        }
+        void Clock_Tick()
+        {
+            Console.WriteLine("Tick! Tack!");
+        }
+
+        void Clock_Alarm()
+        {
+            Console.WriteLine("Alarming");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClock myclock = new MyClock();
+            myclock.clock.AlarmTime = DateTime.Now.AddSeconds(5);
+            myclock.clock.start();
+        }
+    }
+}
---
+++ Homework04/Homework04/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            Node<T> index = head;
+            while (index != null)
+            {
+                action(index.Data);
+                index = index.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                intList.Add(i);
+            }
+            int maxmium = int.MinValue, minimium = int.MaxValue, sum = 0;
+            intList.Foreach(data => Console.WriteLine(data));
+            intList.Foreach(data => {
+                maxmium = maxmium < data ? data : maxmium;
+                minimium = minimium > data ? data : minimium;
+                sum += data;
+            });
+            Console.WriteLine($"MaxValue:{maxmium}; MinValue:{minimium}; Sum:{sum}");
+        }
+    }
+}
